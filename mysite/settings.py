f8e76fe11fb2a1d05d53cda216520b26b21934b6
BangYoon <yoonb720@naver.com>
2018-08-25 00:07:36 +0900

    Change 'Debug' true to flase

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm2r--2i^r5+-i7dhfvfdhf%v$ksrpza+lf!eh@%afn$#7e1%#y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'http://bangyoon.pythonanywhere.com']
-
+ALLOWED_HOSTS = ['127.0.0.1','http://bangyoon.pythonanywhere.com']
 
 # Application definition
 
